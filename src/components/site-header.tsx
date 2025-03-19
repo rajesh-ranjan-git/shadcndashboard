d5ea9a92418ca98575dcc46d01c20ca266316423
Rajesh <rajeshranjan8271@gmail.com>
2025-03-20 03:29:26 +0530

    done with theme

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,19 +1,21 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { ThemeSelector } from "@/components/theme-selector";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+    <header className="flex items-center gap-2 border-b h-(--header-height) group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) transition-[width,height] ease-linear shrink-0">
+      <div className="flex items-center gap-1 lg:gap-2 px-4 lg:px-6 w-full">
         <SidebarTrigger className="-ml-1" />
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
-        <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+        <h1 className="font-medium text-base">Documents</h1>
+        <div className="flex items-center gap-2 ml-auto">
+          {/* <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
               href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
               rel="noopener noreferrer"
@@ -22,9 +24,11 @@ export function SiteHeader() {
             >
               GitHub
             </a>
-          </Button>
+          </Button> */}
+          <ThemeSelector />
+          <ModeToggle />
         </div>
       </div>
     </header>
-  )
+  );
 }
